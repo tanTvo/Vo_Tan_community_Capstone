149245d872b42a27cdcf5de048cadaa0fb128d9e
Tan <tan10tvo@gmail.com>
2024-06-10 17:18:48 -0600

    l/d

--- frontend/com/src/components/Header.jsx
+++ frontend/com/src/components/Header.jsx
@@ -1,5 +1,20 @@
+import { Flex, useColorMode, Image } from "@chakra-ui/react";
+
+
 const Header = () => {
-    return <div>Header</div>;
+    const {colorMode, toggleColorMode} = useColorMode()
+    return <Flex justifyContent={"center"} mt={6} mb={12}>
+
+    <Image 
+        cursor={"pointer"}
+        alt= 'logo'
+        w={24}
+        src={colorMode === "dark" ? "/light-logo.png" : "/dark-logo.png"}
+        onClick={toggleColorMode}
+    />
+
+
+        </Flex>
 };
 
 

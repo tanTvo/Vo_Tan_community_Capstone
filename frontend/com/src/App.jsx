776c3d1fce29939115bc70b483d38d62b3ad706b
Tan <tan10tvo@gmail.com>
2024-06-10 15:13:33 -0600

    header

--- frontend/com/src/App.jsx
+++ frontend/com/src/App.jsx
@@ -2,14 +2,19 @@ import { Button } from "@chakra-ui/button"
 import { Container } from "@chakra-ui/react"
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
+import UserPage from "./pages/UserPage"
+import PostPage from "./pages/PostPage"
+import Header from "./components/Header"
 
 function App() {
   
 
   return (
     <Container maxW='620px'>
+      <Header/>
    <Routes >
     <Route path="/:username" element={<UserPage/>}/>
+    <Route path="/:username/post/:pid" element={<PostPage/>}/>
    </Routes>
     </Container>
   )

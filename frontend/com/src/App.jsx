f063cf04da2f65f08786f727bb344ba4ba2e2f8f
Tan <tan10tvo@gmail.com>
2024-06-10 14:26:08 -0600

    routes

--- frontend/com/src/App.jsx
+++ frontend/com/src/App.jsx
@@ -1,12 +1,17 @@
-
+import { Button } from "@chakra-ui/button"
+import { Container } from "@chakra-ui/react"
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
-     hi
-    </>
+    <Container maxW='620px'>
+   <Routes >
+    <Route path="/:username" element={<UserPage/>}/>
+   </Routes>
+    </Container>
   )
 }
 

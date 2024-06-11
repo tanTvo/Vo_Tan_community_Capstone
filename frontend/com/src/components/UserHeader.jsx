a77b37605313fcfcbee7dadaaf2ecd7b3aa95d1d
Tan <tan10tvo@gmail.com>
2024-06-11 09:13:49 -0600

    userheader

--- frontend/com/src/components/UserHeader.jsx
+++ frontend/com/src/components/UserHeader.jsx
@@ -1,12 +1,30 @@
 import {Box, Flex, Link, Text, VStack } from "@chakra-ui/layout"
 import { Avatar } from "@chakra-ui/avatar";
 import {BsInstagram} from "react-icons/bs"
+import { Portal } from "@chakra-ui/portal";
+import { CgMoreO } from "react-icons/cg";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import { useToast } from "@chakra-ui/toast";
+
 
 
 
 const UserHeader = () => {
-    return(<VStack gap={4} alignItems={"start"}>
-        
+    const toast = useToast()
+    const copyURL = () =>{
+       const currentURL = window.location.href;
+       navigator.clipboard.writeText(currentURL).then(() =>{
+        toast({
+          title: 'Account created.',
+          description: "Profile link copied.",
+          status: 'success',
+          duration: 300,
+          isClosable: true,
+        })
+       });
+    } ;
+    return(
+    <VStack gap={4} alignItems={"start"}>
         
        <Flex justifyContent={"space-between"} w={"full"}>
         <Box>
@@ -44,9 +62,26 @@ const UserHeader = () => {
                 <Box className="icon-container">
                     <BsInstagram size={24} cursor={"pointer"}/>
                 </Box>
+                <Box className="icon-container">
+                    <Menu>
+                    <MenuButton>
+                    <CgMoreO size={24} cursor={"pointer"}/>
+                    </MenuButton>
+                    <Portal>
+                    <MenuList bg={"gray.dark"}>
+                      <MenuItem bg={"gray.dark"} onClick={copyURL}>Copy Link</MenuItem>
+                     </MenuList>
+                    </Portal>
+                    </Menu>
+                </Box>
             </Flex>
        </Flex>
-        
+        <Flex w={"full"}>
+            <Flex flex={1} borderBottom={"1.5px solid white"} justifyContent={"center"} pb="3" cursor={"pointer"}>
+                <Text fontWeight={"bold"}>Crumbs</Text>
+            </Flex>
+            <Flex  flex={1} borderBottom={"1px solid gray"} justifyContent={"center"} color={"gray.light"} pb="3" cursor={"pointer"}>Replies</Flex>
+        </Flex>
             </VStack>
 
     )

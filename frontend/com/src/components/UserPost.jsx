79347c4eb8dcd1b8ef9ce05d03eb3a895b7c8cbf
Tan <tan10tvo@gmail.com>
2024-06-11 11:26:33 -0600

    actions

--- frontend/com/src/components/UserPost.jsx
+++ frontend/com/src/components/UserPost.jsx
@@ -1,6 +1,7 @@
 import { Avatar } from '@chakra-ui/avatar'
-
-import { Box, Flex } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/image'
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { BsThreeDots } from 'react-icons/bs'
 import {Link} from 'react-router-dom'
 
 
@@ -29,8 +30,9 @@ const UserPost = () => {
                 name='john doe'
                 src='https://bit.ly/sage-adebayo'
                 position={"absolute"}
-                top={"0px"}
-                left="15px"
+                bottom={"0px"}
+                
+                right="-5px"
                 padding={"2px"}
                 />
                    <Avatar
@@ -38,11 +40,29 @@ const UserPost = () => {
                 name='john doe'
                 src='https://bit.ly/code-beast'
                 position={"absolute"}
-                top={"0px"}
-                left="15px"
+                bottom={"0px"}
+                left="4px"
                 padding={"2px"}
                 />
             </Box>
+        </Flex>
+        <Flex flex={1} flexDirection={"column"} gap={2}>
+        <Flex justifyContent={"space-between"} w={"full"}>
+            <Flex w={"full"} alignItems={"center"} >
+                <Text fontSize={"sm"} fontWeight={"bold"}>Tan vo</Text>
+                <Image src='/verified.png' w={4} h={4} ml={1} />
+            </Flex>
+            <Flex gap={4} alignItems={"center"}>
+                <Text fontStyle={"sm"} color={"gray.light"}>1d</Text>
+                <BsThreeDots />
+            </Flex>
+
+        </Flex>
+            <Text fontSize={"sm"}>This is my first post</Text>
+            <Box borderRadius={6} overflow={"hidden"} border={"1px solid "} borderColor={"gray.light"}>
+                <Image src='/post1.png' w={"full"}/>
+            </Box>
+
         </Flex>
         </Flex>
         

b8fb21d41da7ca5f556ab11f3172c013b138f8b8
Tan <tan10tvo@gmail.com>
2024-06-11 15:29:10 -0600

    post page

--- frontend/com/src/pages/PostPage.jsx
+++ frontend/com/src/pages/PostPage.jsx
@@ -1,9 +1,12 @@
-import { Avatar, Flex } from "@chakra-ui/react";
+import { Avatar, Box, Button, Divider, Flex, Image, Text } from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
+import { useState } from "react";
+import Actions from "../components/Actions";
 
 
 
 const PostPage = () => {
+    const [liked, setLiked] = useState(false)
     return <>
     <Flex>
         <Flex w={"full"} alignItems={"center"} gap={3}>
@@ -20,7 +23,34 @@ const PostPage = () => {
             <BsThreeDots/>
         </Flex>
     </Flex>
-    <Text>Lets talk about life</Text>
+    <Text my={3}>Lets talk about life</Text>
+    <Box borderRadius={6} overflow={"hidden"} border={"1px solid "} borderColor={"gray.light"}>
+                <Image src={"post1.png"} w={"full"}/>
+            </Box>
+            <Flex gap={3} my={3}>
+                <Actions liked={liked} setLiked={setLiked} />
+            </Flex>
+            <Flex gap={2} alignItems={"center"}>
+                <Text color={"gray.light"} fontSize={"sm"}>200 replies</Text>
+                <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+                <Text color={"gray.light"} fontSize={"sm"}>
+                    {200 + (liked ? 1 : 0) } likes
+                </Text>
+            </Flex>
+            <Divider my={4}/>
+
+            <Flex>
+                <Flex gap={2} alignItems={"center"}>
+                    <Text fontSize={"2x1"}>👋</Text>
+                    <Text color={"gray.light"}>Get the app to like, reply, and post</Text>
+
+                </Flex>
+                <Button>
+                    Get
+                </Button>
+            </Flex>
+
+            <Divider my={4}/>
     </>
 };
 

6b32613a19dbaca8e52db050056b7c2bc0b5d234
Tan <tan10tvo@gmail.com>
2024-06-11 21:48:40 -0600

    util/backend server

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signupUser } from "../controllers/userController";
+import { signupUser } from "../controllers/userController.js";
 
 const router = express.Router();
 

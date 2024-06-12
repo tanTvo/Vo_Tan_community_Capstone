f5da73fb01ec2cd15e7295876789f55bdf40fa4d
Tan <tan10tvo@gmail.com>
2024-06-12 17:38:54 -0600

    updated comment

--- frontend/com/src/components/Comment.jsx
+++ frontend/com/src/components/Comment.jsx
@@ -1,45 +1,22 @@
-import { Avatar, Divider, Flex } from "@chakra-ui/react"
-import { useState } from "react"
-import { BsThreeDots } from "react-icons/bs"
+import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
 
-const Comment = () => {
-    const [liked, setLiked] = useState()
-    return(
-       <>
-       <Flex gap={4} py={2} my={2} w={"full"}>
-            <Avatar src="/tan-avatar.png" size={"sm"}/>
-            <Flex gap={1} w={"full"} flexDirection={"column"}>
-                <Flex w={"full"} justifyContent={ "space-between"} alignItems={"center"}>
-                    <Text fontSize="sm" fontWeight="bold">Tan Vo</Text>
-                    <Flex>
-                        <Text fontSize={"sm"} color={"gray.light"}> 1d</Text>
-                        <BsThreeDots/>
-                    </Flex>
-                </Flex>
-                <Text>Hey this looks great!</Text>
-                <Actions liked={liked} setLiked={setLiked}/>
-                <Text fontSize={"sm"} color={"gray.light"}>
-                    {100 + (liked ? 1 : 0)} likes
-                </Text>
-            </Flex>
+const Comment = ({ reply, lastReply }) => {
+	return (
+		<>
+			<Flex gap={4} py={2} my={2} w={"full"}>
+				<Avatar src={reply.userProfilePic} size={"sm"} />
+				<Flex gap={1} w={"full"} flexDirection={"column"}>
+					<Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
+						<Text fontSize='sm' fontWeight='bold'>
+							{reply.username}
+						</Text>
+					</Flex>
+					<Text>{reply.text}</Text>
+				</Flex>
+			</Flex>
+			{!lastReply ? <Divider /> : null}
+		</>
+	);
+};
 
-
-
-
-       </Flex>
-       <Divider/>
-       <Comment />
-       <Comment />
-       <Comment />
-       
-       
-       </>
-    )
-}
-
-
-
-
-
-
-export default Comment
+export default Comment;

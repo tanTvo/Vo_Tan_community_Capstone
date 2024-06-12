e39f094b5168436610b99f33253e63a94733c2a0
Tan <tan10tvo@gmail.com>
2024-06-11 18:35:52 -0600

    files

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import dotenv from "dotenv";
 
 
+dotenv.config();
+const app = express();
 
-const app = express()
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));

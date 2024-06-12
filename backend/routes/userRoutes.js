9f1a3b65a9e27fbf8d75a96bfaa5552340dd7127
Tan <tan10tvo@gmail.com>
2024-06-12 00:01:36 -0600

    middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { signupUser, loginUser } from "../controllers/userController.js";
+import { followUnFollowUser, signupUser, loginUser, logoutUser } from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser)
 
 
 export default router; 

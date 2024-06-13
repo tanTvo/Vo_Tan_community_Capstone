35c118d1b5d2bbe03bc73d7e3b49ef0550c645af
Tan <tan10tvo@gmail.com>
2024-06-12 21:14:33 -0600

    message routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import connectDB from './db/connectDB.js';
-import cookieParser from 'cookie-parser';
-import userRoutes from "./routes/userRoutes.js"
-import postRoutes from "./routes/postRoutes.js"
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 
 
@@ -29,6 +30,6 @@ app.use(cookieParser());
 //Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
-
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));

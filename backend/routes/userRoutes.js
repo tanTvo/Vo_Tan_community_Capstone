6e7d563166cb9f87b7f5081114df76cc7fc99bac
Tan <tan10tvo@gmail.com>
2024-06-12 23:54:39 -0600

    routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,25 @@
 import express from "express";
-import { followUnFollowUser, getUserProfile, signupUser, loginUser, logoutUser, updateUser } from "../controllers/userController.js";
+import {
+	followUnFollowUser,
+	getUserProfile,
+	loginUser,
+	logoutUser,
+	signupUser,
+	updateUser,
+	getSuggestedUsers,
+	freezeAccount,
+} from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/profile/:query", getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.post("/follow/:id", protectRoute, followUnFollowUser)
-router.put("/update/:id", protectRoute, updateUser)
+router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
+router.put("/update/:id", protectRoute, updateUser);
+router.put("/freeze", protectRoute, freezeAccount);
 
-
-export default router; 
+export default router;
